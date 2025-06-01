299d9b9873b87702710754481198ac0f59b4f297
Morissey Monyl Carolino <morissey2005@gmail.com>
2025-06-02 03:21:36 +0400

    Changed image routing

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
     
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://crochetnbeads.onrender.com/images/${req.file.filename}`
     });
 });
 
