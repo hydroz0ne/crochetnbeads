0f4835fc72aae4fed577b6d2d7bf763ab782faf6
Morissey Monyl Carolino <morissey2005@gmail.com>
2025-06-02 17:45:36 +0400

    Reverted previous changes.

--- frontend/src/Components/Product/Product.jsx
+++ frontend/src/Components/Product/Product.jsx
@@ -4,17 +4,13 @@ import { Link } from "react-router-dom";
 
 const Product = (props) => {
     return (
-        <Link 
-            to={`/${props.category}/${props.id}`} 
-            onClick={() => window.scrollTo(0, 0)}
-            className="product-link"
-        >
-            <div className="product">
-                <img src={props.image} alt={props.name} />
-                <h2>{props.name}</h2>
-                <p>{props.price} AED</p>
-            </div>
-        </Link>
+        <div className="product">
+            <Link to={`/${props.category}/${props.id}`}>
+                <img onClick={() => window.scrollTo(0, 0)} src={props.image} alt={props.name} />
+            </Link>
+            <h2>{props.name}</h2>
+            <p>{props.price} AED</p>
+        </div>
     );
 }
 
